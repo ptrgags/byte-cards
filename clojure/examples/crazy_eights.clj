3f280ec21ea2bad8e3a47d63fb495c9324d5db41
Peter Gagliardi <ptrgags@live.com>
2015-11-14 09:59:25 -0500

    Crazy Eights: valid cards and drawing cards
    Made a function to check which cards in a hand can be
    played
    
    Also made a function to move the top card from the deck
    to the hand in functional style.

--- clojure/examples/crazy_eights.clj
+++ clojure/examples/crazy_eights.clj
@@ -18,5 +18,54 @@
 		[table deck] (deck-draw deck)]
 		[hand1 hand2 table deck]))
 
-;Test setup
-(println (setup (deck-create)))
+;Have a player draw a card
+(defn player-draw [hand deck]
+	(let [[top deck] (deck-draw deck)]
+		[(concat hand top) deck]))
+
+;Split cards into cards that can be played
+;and cards that cannot given the top card
+(defn valid-cards [hand table]
+	(let [top-card (first table)
+		top-rank (card-rank top-card)
+		top-suit (card-suit top-card)
+		card-validity (group-by (fn [x] 
+			(or (= (card-rank x) 8)
+				(= (card-rank x) top-rank)
+				(= (card-suit x) top-suit)))
+			hand)]
+		[(get card-validity true) (get card-validity false)]))
+
+;Enumerate a list
+(defn enumerate [coll]
+	(map vector (range) coll))
+
+;Display a hand
+(defn hand-show [hand]
+	(doseq [[i x] (enumerate hand)]
+		(println (format "%d) %s" i (card-name x)))))
+
+;Test what we have so far
+
+(println "Choosing cards to play====")
+(let [
+	[hand1 hand2 table deck] (setup (deck-create))
+	[valid invalid] (valid-cards hand1 table)
+	valid-names (map card-name valid)
+	invalid-names (map card-name invalid)
+	table-names (map card-name table)]
+	(println "Top card on pile:")
+	(println (card-name (first table)))
+	(println "Valid cards:")
+	(hand-show valid)
+	(println "Invalid cards:")
+	(hand-show invalid))
+
+(println "Drawing cards=====")
+(let [
+	[hand1 hand2 table deck] (setup (deck-create))
+	[hand1 deck] (player-draw hand1 deck)]
+	(println "Player 1:")
+	(hand-show hand1)
+	(println "Player 2:")
+	(hand-show hand2))
