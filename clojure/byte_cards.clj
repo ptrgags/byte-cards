e1aa7a1cc2b2e58d344949b413208d2085185df0
Peter Gagliardi <ptrgags@live.com>
2015-11-28 08:27:49 -0500

    Fix docstrings in byte_cards.clj

--- clojure/byte_cards.clj
+++ clojure/byte_cards.clj
@@ -7,27 +7,30 @@
 (def RANK_NAMES "0123456789ABCDEF")
 (def SUIT_NAMES ["Spades" "Hearts" "Clubs" "Diamonds"])
 
-;Rank of the card is the last 4 bits
-(defn card-rank [card] 
+(defn card-rank
+  "Rank of the card is the lowest 4 bits"
+  [card] 
   (bit-and card (- RANKS 1)))
 
-;Suit of the card is the second set of 2 bits from the left
-(defn card-suit [card]
+(defn card-suit 
+  "Suit of the card is the second set of 2 bits from the left"
+  [card]
   (bit-and (unsigned-bit-shift-right card 4) 
            (- SUITS 1)))
 
-;Wild number is the first 2 bits from the left
-(defn card-wild [card]
+(defn card-wild 
+  "Wild number is the first 2 bits from the left"
+  [card]
   (bit-and (unsigned-bit-shift-right card 6) 
            (- WILDS 4)))
 
-;display the card as "<rank> of <suit>"
 (defn card-name 
+  "Display the card as '<rank> of <suit>'"
   ([card]
    (format "%s of %s"
-	       (get RANK_NAMES (card-rank card))
-	       (get SUIT_NAMES (card-suit card))))
+     (get RANK_NAMES (card-rank card))
+	 (get SUIT_NAMES (card-suit card))))
   ([card rank-names suit-names]
    (format "%s of %s" 
-           (get rank-names (card-rank card)) 
-		   (get suit-names (card-suit card)))))
+     (get rank-names (card-rank card)) 
+	 (get suit-names (card-suit card)))))
