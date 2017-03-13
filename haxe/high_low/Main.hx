e0f9618aca0b6d2c713db13af51e2dd1b6e74a94
Peter Gagliardi <ptrgags@live.com>
2017-03-13 18:35:35 -0400

    Add config file for High Low
    Add a JSON config file for High Low

--- haxe/high_low/Main.hx
+++ haxe/high_low/Main.hx
@@ -2,14 +2,40 @@ package high_low;
 
 import high_low.HighLowGame;
 
+typedef Config = {
+    var player_type:String;
+    var num_games:Int;
+}
+
+class ConfigLoader {
+    private var fname:String;
+
+    public function new(fname:String) {
+        this.fname = fname;
+    }
+
+    public function load():Config {
+        var json = sys.io.File.getContent(fname);
+        var conf:Config = haxe.Json.parse(json);
+        return conf;
+    }
+}
+
 class Main {
     static public function main():Void {
-        var game = new HighLowGame();
+        //Load config
+        var conf_fname = Sys.args()[0];
+        var loader = new ConfigLoader(conf_fname);
+        var config = loader.load();
 
+        //Play games over and over again
+        var game = new HighLowGame();
         var total_turns = 0.0;
-        var N = 10000;
-        for (i in 0...N)
+        var N = config.num_games;
+        for (i in 0...N) {
+            Sys.println('== Game ${i + 1} ======================');
             total_turns += game.play();
+        }
         var avg = total_turns / N;
         Sys.println('Player lasted an average of $avg turns');
     }
